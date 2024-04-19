16c715ffd61ae79be5c7970cfd304ab3773c9795
Sravani <nagamangalamsravanirao@gmail.com>
2024-04-19 12:27:03 +0530

    first commit

---
+++ src/Add.java
@@ -0,0 +1,13 @@
+public class Add {
+    public static void main(String[] args) {
+        // Declare and initialize two numbers
+        int num1 = 5;
+        int num2 = 10;
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
